04957487a9cadac51f8ea8affd2d64123840f87f
Hasit Bhatt <hasit.p.bhatt@gmail.com>
2020-09-20 17:25:59 +0530

    Add goquery to simplify querying

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/hasitpbhatt/amazon-free-books
 
 go 1.14
 
-require golang.org/x/net v0.0.0-20200602114024-627f9648deb9
+require (
+	github.com/PuerkitoBio/goquery v1.5.1
+	github.com/json-iterator/go v1.1.10
+	golang.org/x/net v0.0.0-20200602114024-627f9648deb9
+	gopkg.in/yaml.v1 v1.0.0-20140924161607-9f9df34309c0
+	gopkg.in/yaml.v2 v2.3.0
+)
--- main.go
+++ main.go
@@ -1,18 +1,35 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
+	"compress/gzip"
 	"encoding/csv"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/http/cookiejar"
+	"net/url"
+	"os"
+	"os/exec"
 	"strconv"
 	"strings"
+	"time"
 
+	"github.com/PuerkitoBio/goquery"
+	jsoniter "github.com/json-iterator/go"
 	"golang.org/x/net/html"
+	"gopkg.in/yaml.v1"
 )
 
+var json = jsoniter.ConfigCompatibleWithStandardLibrary
+var _client = &http.Client{}
+var _buf = &bytes.Buffer{}
+
+var base = "https://www.amazon.in/s?i=digital-text&bbn=1634753031&rh=n%3A1571277031%2Cn%3A1571278031%2Cn%3A1634753031%2Cp_36%3A-1%2Cp_n_feature_three_browse-bin%3A10837939031%7C10837942031%7C11301931031&dc&qid=1600193250&rnid=10837938031&ref=sr_nr_p_n_feature_three_browse-bin_5"
+
+const _startPage = 1
 const _maxPagesToScroll = 400 // can't be more than 400
 
 var (
@@ -22,11 +39,122 @@ var (
 	_invalid   = map[string]bool{}
 )
 
+var config = struct {
+	CSRF         string `yaml:"csrf"`
+	CSRFCheckout string `yaml:"csrfCheckout"`
+	Cookies      string `yaml:"cookies"`
+}{}
+
 type book struct {
 	asin  string
 	href  string
 	title string
 	stars string
+}
+
+func getCookiesIn() []*http.Cookie {
+	return getCookies(config.Cookies)
+}
+
+func getCookies(cookieStr string) []*http.Cookie {
+	rawRequest := fmt.Sprintf("GET / HTTP/1.0\r\n%s\r\n\r\n", cookieStr)
+
+	req, _ := http.ReadRequest(bufio.NewReader(strings.NewReader(rawRequest)))
+
+	return req.Cookies()
+}
+
+func setCookies() {
+	u, _ := url.Parse("https://www.amazon.in")
+	_jar.SetCookies(u, getCookiesIn())
+}
+
+func setBrowserHeader(req *http.Request) {
+	req.Header.Set("user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.100 Safari/537.36")
+}
+
+var cmdVarArray []string
+
+func checkout() {
+	fmt.Println("Checking out books...")
+	cmdVarArray = []string{
+		`--verbose`,
+		`https://www.amazon.in/api/bifrost/acquisitions/v1/collections/midgard/bundles/Active`,
+		`-H`,
+		`'content-type: application/x-www-form-urlencoded'`,
+		`-H`,
+		`'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.100 Safari/537.36'`,
+		`-H`,
+		fmt.Sprintf(`$'%s'`, config.Cookies),
+		`--data`,
+		fmt.Sprintf(`'csrf=%s&x-client-id=ebook-cart&payment.mode=checkout&proceedToCheckout=1'`, config.CSRFCheckout),
+		`--compressed`,
+	}
+	cmd := exec.Command("curl", cmdVarArray...)
+	out, err := exec.Command("/bin/bash", "-c", cmd.String()).CombinedOutput()
+	if err != nil {
+		fmt.Println(exec.Command("/bin/bash", "-c", cmd.String()).String())
+		fmt.Println(string(out))
+		log.Fatal(err)
+	}
+}
+
+func addToCart(c *http.Client, asin string) (int, float64, bool) {
+	fmt.Println("adding to cart...")
+	jsonStr := fmt.Sprintf(`{"asin":"%s","csrf":"%s","action":"add"}`, asin, config.CSRF)
+	req, err := http.NewRequest(http.MethodPost, "https://www.amazon.in/api/bifrost/bundles/v1/collections/midgard/bundles/Active?&x-client-id=ebook-cart", bytes.NewBufferString(jsonStr))
+	if err != nil {
+		panic(err)
+	}
+
+	setBrowserHeader(req)
+	req.Header.Set("content-type", "application/json")
+	req.Header.Set("accept", "application/json")
+	req.Header.Set("accept-encoding", "gzip, deflate, br")
+	req.Header.Set("accept-language", "en-US,en;q=0.9,gu;q=0.8")
+	req.Header.Set("x-requested-with", "XMLHttpRequest")
+
+	resp, err := c.Do(req)
+
+	r, err := gzip.NewReader(resp.Body)
+	if err != nil {
+		log.Fatalf("unable to read gzip")
+	}
+	body, err := ioutil.ReadAll(r)
+	if err != nil {
+		log.Fatalf("unable to read body")
+	}
+
+	type x struct {
+		Resources struct {
+			DetailedStatus string `json:"detailedStatus"`
+			ReferenceData  struct {
+				Bundle struct {
+					Summary struct {
+						TotalItemCount               float64 `json:"totalItemCount"`
+						TotalPriceWithTaxAndDiscount struct {
+							Amount float64 `json:"amount"`
+						} `json:"totalPriceWithTaxAndDiscount"`
+					} `json:"summary"`
+				} `json:"bundle"`
+			} `json:"referenceData"`
+		} `json:"resources"`
+	}
+	m := x{}
+	err = json.Unmarshal(body, &m)
+	if err != nil {
+		log.Fatal("Unable to marshal")
+	}
+	fmt.Println(m)
+	details := m.Resources.ReferenceData.Bundle.Summary
+	fmt.Println(asin, m.Resources.DetailedStatus)
+	alreadyOwned := m.Resources.DetailedStatus == "alreadyOwned" || m.Resources.DetailedStatus == "ok"
+	if m.Resources.DetailedStatus == "limitExceeded" {
+		time.Sleep(5 * time.Second)
+		checkout()
+		return addToCart(c, asin)
+	}
+	return int(details.TotalItemCount), details.TotalPriceWithTaxAndDiscount.Amount, alreadyOwned
 }
 
 func init() {
@@ -37,6 +165,16 @@ func init() {
 	_jar = jar
 	fill("free.txt", _purchased)
 	fill("invalid.txt", _invalid)
+
+	yamlFile, err := ioutil.ReadFile("config.yaml")
+	if err != nil {
+		fmt.Printf("Error reading YAML file: %s\n", err)
+		return
+	}
+
+	err = yaml.Unmarshal(yamlFile, &config)
+	config.Cookies = strings.TrimRight(config.Cookies, "\n")
+	setCookies()
 }
 
 func fill(path string, m map[string]bool) {
@@ -50,35 +188,54 @@ func fill(path string, m map[string]bool) {
 		panic(err)
 	}
 	for _, record := range records {
-		m[record[0]] = true
+		m[strings.Split(record[0], "#")[0]] = true
 	}
 }
 
-const base = "https://www.amazon.in/s?i=digital-text&bbn=1634753031&rh=n%3A1571277031%2Cn%3A1571278031%2Cn%3A1634753031%2Cp_36%3A-1%2Cp_n_feature_three_browse-bin%3A10837939031%7C10837942031%7C11301931031%2Cp_72%3A1318476031&dc&qid=1594215302&rnid=1318475031&ref=sr_nr_p_72_1"
+func getNumberOfPages(c *http.Client) int {
+	bts := getPage(c, base)
+	ioutil.WriteFile("ssss.html", bts, 0777)
+	d, err := goquery.NewDocumentFromReader(bytes.NewBuffer(bts))
+	if err != nil {
+		log.Fatal(err)
+	}
+	d = goquery.NewDocumentFromNode(d.Find(".a-pagination").Get(0).LastChild.PrevSibling.PrevSibling)
+	htmlD, _ := d.Html()
+	pages, _ := strconv.Atoi(htmlD)
+	fmt.Println("Max pages found to be:", pages)
+	return pages
+}
 
 func main() {
 	c := &http.Client{
 		Jar: _jar,
 	}
-	_ = c
-	for i := 1; i <= _maxPagesToScroll; i++ {
-		fmt.Printf("%d,", i)
+	_client = c
+	startPage := _startPage
+	maxPagesToScroll := _maxPagesToScroll
+	if len(os.Args) > 1 {
+		base = os.Args[1]
+	}
+	pages := getNumberOfPages(c)
+	if len(os.Args) > 2 {
+		startPage, _ = strconv.Atoi(os.Args[2])
+	}
+	if len(os.Args) > 3 {
+		maxPagesToScroll, _ = strconv.Atoi(os.Args[3])
+	} else {
+		maxPagesToScroll = pages
+	}
+	checkout()
+	for i := startPage; i <= maxPagesToScroll; i++ {
+		fmt.Println("%d,", i)
 		b := foo(c, int64(i))
-		// b, err := ioutil.ReadFile("page" + strconv.FormatInt(int64(i), 10) + ".html")
-		// if err != nil {
-		// 	panic(err)
-		// }
 		if err := parse2(b); err != nil {
 			panic(err)
 		}
-		// time.Sleep(4 * time.Second)
 	}
-	fmt.Println()
-	for _, book := range _m {
-		fmt.Println(book.title)
-		fmt.Println(book.href, book.stars)
-		fmt.Println("----------------------")
-	}
+	fmt.Println("Found", len(_m), "new books")
+	checkout()
+	ioutil.WriteFile("downloaded.txt", _buf.Bytes(), 0777)
 }
 
 func parse2(b []byte) error {
@@ -86,82 +243,73 @@ func parse2(b []byte) error {
 	if err != nil {
 		return err
 	}
-	var f func(*html.Node)
-	f = func(n *html.Node) {
-		if n.Data == "div" {
-			if book := getBook(n); book != nil {
-				if !_purchased[book.asin] && !_invalid[book.asin] {
-					_m[book.asin] = *book
-				}
+	goq := goquery.NewDocumentFromNode(n)
+	goq.Find("div[data-asin]").Each(
+		func(i int, s *goquery.Selection) {
+			asin, exists := s.Attr("data-asin")
+			if !exists || asin == "" {
 				return
 			}
-		}
-		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			f(c)
-		}
-	}
-	f(n)
+			if _purchased[asin] || _invalid[asin] {
+				return
+			}
+			b := book{
+				asin: asin,
+			}
+			s.Find("a[href]").Each(
+				func(j int, s *goquery.Selection) {
+					href, exists := s.Attr("href")
+					if !exists || !strings.Contains(href, "/") {
+						return
+					}
+					b.href = "https://amazon.in" + href
+					rating := ""
+					ratingNodes := s.Find(".a-icon-alt")
+					if ratingNodes.Size() > 0 {
+						rating = ratingNodes.Text()
+						b.stars = rating
+					}
+				},
+			)
+			z := `₹0 to buy`
+			valid := false
+			s.Find(`span[dir="auto"]`).Each(
+				func(j int, s *goquery.Selection) {
+					if !strings.Contains(s.Text(), z) {
+						return
+					}
+					valid = true
+					fmt.Println(asin, s.Text())
+				},
+			)
+			if b.href != "" {
+				_m[asin] = b
+				book := b
+				items, price, alreadyOwned := addToCart(_client, book.asin)
+				if alreadyOwned {
+					_buf.WriteString(book.asin + "\n")
+				}
+				if price > 0 {
+					log.Fatalf("Non-free book found")
+				}
+				if items == 10 {
+					checkout()
+				}
+			} else {
+				fmt.Println("href is empty?", b.href, valid)
+			}
+		},
+	)
 	return nil
 }
 
-func getBook(n *html.Node) *book {
-	var asin *string
-	asin = getString(n, "data-asin")
-	if asin == nil {
-		return nil
-	}
-	c := n.FirstChild                                     // div [{ class sg-col-inner}]
-	l := c.FirstChild.NextSibling                         // span [{ cel_widget_id MAIN-SEARCH_RESULTS} { class celwidget slot=MAIN template=SEARCH_RESULTS widgetId=search-results}]
-	m := l.FirstChild.NextSibling                         // div [{ class s-include-content-margin s-border-bottom s-latency-cf-section}]
-	p := m.FirstChild.NextSibling                         // div [{ class a-section a-spacing-medium}]
-	g := p.FirstChild.NextSibling                         // empty row
-	g = g.NextSibling.NextSibling                         // original row : div [{ class sg-row}]
-	q := g.FirstChild.NextSibling.NextSibling.NextSibling // div [{ class sg-col-4-of-12 sg-col-8-of-16 sg-col-16-of-24 sg-col-12-of-20 sg-col-24-of-32 sg-col sg-col-28-of-36 sg-col-20-of-28}]
-	// print("q", q)
-	t := q.FirstChild // div [{ class sg-col-inner}]
-	// print("t", t)
-	k := t.FirstChild.NextSibling
-	k1 := k.FirstChild.NextSibling
-	k2 := k1.FirstChild
-	k3 := k2.FirstChild.NextSibling // div [{ class a-section a-spacing-none}]
-	k4 := k3.FirstChild.NextSibling.FirstChild.NextSibling
-	href := getString(k4, "href")
-
-	b := &book{
-		asin:  *asin,
-		href:  "https://amazon.in" + *href,
-		title: k4.FirstChild.NextSibling.FirstChild.Data,
-	}
-	if k3.NextSibling.NextSibling != nil {
-		k31 := k3.NextSibling.NextSibling // div [{ class a-section a-spacing-none a-spacing-top-micro}]
-		k32 := k31.FirstChild.NextSibling.FirstChild.NextSibling
-		stars := getString(k32, "aria-label")
-		b.stars = *stars
-	}
-	return b
-}
-
-func getString(n *html.Node, name string) *string {
-	for _, a := range n.Attr {
-		if a.Key == name && a.Val != "" {
-			return &a.Val
-		}
-	}
-	return nil
-}
-
-func foo(c *http.Client, page int64) (bytes []byte) {
-	url := base
-	if page > 1 {
-		url = base + "&page=" + strconv.FormatInt(page, 10)
-	}
-
+func getPage(c *http.Client, url string) []byte {
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		panic(err)
 	}
 
-	req.Header.Set("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36")
+	setBrowserHeader(req)
 
 	resp, err := c.Do(req)
 	if err != nil {
@@ -169,32 +317,20 @@ func foo(c *http.Client, page int64) (bytes []byte) {
 	}
 	b, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	// ioutil.WriteFile("page"+strconv.FormatInt(page, 10)+".html", b, 0777)
 	return b
 }
 
+func foo(c *http.Client, page int64) (bytes []byte) {
+	url := base
+	if page > 1 {
+		url = base + "&page=" + strconv.FormatInt(page, 10)
+	}
+	return getPage(c, url)
+}
+
 func print(title string, n *html.Node) {
 	fmt.Println(title, n.Data, n.DataAtom.String(), n.Attr)
 }
-
-/* Autoadd cart: not working
-for (var i=1;i<10;i++) {
-  var x = window.open('https://amazon.in/dp/' + arr[i]);
-  setTimeout(2000);
-  console.log("x",x)
-  var y = x.opener.document.getElementById('add-to-ebooks-cart-button');
-  y.click();
-  x.close();
-}
-*/
-
-/* Open pages
-for(var i=0;i<arr.length;i++) {
-  if( (i%10) == 0) {
-    alert("aa");
-  }
-  window.open("https://amazon.in/dp/" + arr[i], "_blank");
-}
-*/
